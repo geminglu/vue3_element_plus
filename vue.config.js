59bc9d6e7d03cf52532b66dc37b5176dad7253a4
minglu_ge <geminglu199698@outlook.com>
2023-05-29 22:20:12 +0800

    feat: 反响代理配置

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ const AutoImport = require('unplugin-auto-import/webpack');
 const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
 const path = require('path');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -14,10 +13,16 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
+    port: 8081,
     onBeforeSetupMiddleware:
-      process.env.VUE_APP_ENV === 'mock'
-        ? require('./mock/mock-server')
-        : () => {},
+      process.env.VUE_APP_ENV === 'mock' ? require('./mock/mock-server') : () => {},
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
   },
   configureWebpack: {
     resolve: {
@@ -42,5 +47,19 @@ module.exports = defineConfig({
     if (process.env.use_analyzer) {
       config.plugin('webpack-bundle-analyzer').use(BundleAnalyzerPlugin);
     }
+
+    // 内置的svg处理排除指定目录下的文件
+    config.module.rule('svg').exclude.add(resolve('src/assets/svg')).end();
+
+    config.module
+      .rule('svg-sprite-loader')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/svg'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      });
   },
 });
