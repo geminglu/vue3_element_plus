03f011e006f4c00cb79400789f47ad5b3e20757b
minglu_ge <geminglu199698@outlook.com>
2023-06-15 15:20:37 +0800

    feat:  svg组件封装

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="svg-icon">
+    <use :xlink:href="`#icon-${props.name}`" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+// defineOptions({
+//   name: 'Main',
+// });
+
+type Props = {
+  name: string;
+};
+const props = withDefaults(defineProps<Props>(), {
+  name: '',
+});
+</script>
+
+<style lang="less" scoped>
+.svg-icon {
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  overflow: hidden;
+  vertical-align: -0.15em;
+  fill: currentColor;
+}
+</style>
