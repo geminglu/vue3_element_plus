158ab14cdd2b97056d33674a0303138aece6ecab
minglu_ge <geminglu199698@outlook.com>
2023-06-30 15:00:24 +0800

    feat: 路由缓存

--- src/router/routerGuard.ts
+++ src/router/routerGuard.ts
@@ -32,6 +32,27 @@ export default function routerGuard(router: Router) {
       next({ ...to, replace: true });
       return;
     }
+    // 设置路由缓存
+    const appStore = useAppStore();
+
+    // 自动缓存配置
+    if (from.meta?.keepAlive instanceof Array && to.name) {
+      if (from.meta.keepAlive.includes(to.name as string)) {
+        appStore.setkeepAlives(from.name as string);
+      } else {
+        appStore.delkeepAlives(from.name as string);
+      }
+    } else if (typeof from.meta?.keepAlive === 'boolean' && from.meta?.keepAlive) {
+      appStore.setkeepAlives(from.name as string);
+    } else if (typeof from.meta?.keepAlive === 'boolean' && !from.meta?.keepAlive) {
+      appStore.delkeepAlives(from.name as string);
+    }
+
+    if (to.meta?.keepAlive instanceof Array) {
+      if (!to.meta?.keepAlive.includes(from.name as string)) {
+        appStore.delkeepAlives(to.name as string);
+      }
+    }
 
     next();
   });
---
+++ src/store/modules/app.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+export interface appType {
+  /** 菜单是否展开 */
+  menuCollapse: boolean;
+  /**
+   * 被缓存起来的路由组件
+   */
+  keepAlives: string[];
+}
+
+const useAppStore = defineStore('appStore', {
+  state: (): appType => ({
+    menuCollapse: true,
+    keepAlives: [],
+  }),
+  persist: true,
+  actions: {
+    /**
+     * 设置缓存
+     */
+    setkeepAlives(value: string) {
+      !this.keepAlives.includes(value) && this.keepAlives.push(value);
+    },
+
+    /**
+     * 删除缓存
+     */
+    delkeepAlives(value: string) {
+      this.keepAlives.includes(value) && this.keepAlives.splice(this.keepAlives.indexOf(value), 1);
+    },
+
+    /**
+     * 切换暗黑主题
+     */
+    switchoverDark() {
+      this.layout.darkTheme = !this.layout.darkTheme;
+    },
+  },
+});
+
+export default useAppStore;
