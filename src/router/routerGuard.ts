00cab514bab01f3bd6884a7a310d47af9d137a72
minglu_ge <geminglu199698@outlook.com>
2023-08-31 23:54:59 +0800

    feat: 当前路由名称是NotFound并且在动态路由中存在就跳转到登录页面

--- src/router/routerGuard.ts
+++ src/router/routerGuard.ts
@@ -1,9 +1,11 @@
-import type { Router, RouteRecordRaw, RouteLocationNormalized } from 'vue-router';
+import type { Router, RouteRecordRaw } from 'vue-router';
 import usePermissioStore from '@/store/modules/permission';
 import useUserStore from '@/store/modules/user';
 import NProgress from '@/utils/nprogress';
+import { createRouter, createWebHistory } from 'vue-router';
 import store from '@/store';
 import useAppStore from '@/store/modules/app';
+import asyncRoutes from './routes/modules/sync';
 
 export default function routerGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
@@ -14,6 +16,19 @@ export default function routerGuard(router: Router) {
     if (to.name === 'SignIn') {
       next();
       return;
+    }
+
+    // 如果当前路由名称是NotFound并且在动态路由中存在就跳转到登录页面
+    // 如果这条路由要是在动态路由中存在并且路由名称为NotFound说明这条路由还没有加载，也就是用户还没有登录，需要跳转到登录页完成登录
+    if (to.name === 'NotFound' && !userStore.access_token) {
+      const routes = createRouter({
+        routes: asyncRoutes as unknown as RouteRecordRaw[],
+        history: createWebHistory(),
+      }).getRoutes();
+      if (routes.filter((item) => item.path === to.path).length) {
+        next({ name: 'SignIn', query: { to: to.path } });
+        return;
+      }
     }
 
     // 如果已经登陆并且没有菜单就要获取菜单
